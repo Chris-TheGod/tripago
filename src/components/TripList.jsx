926bfcca66280ae98104c873a5f52b28ce4a197c
Kristjan Lääne <xonproject@gmail.com>
2023-09-27 17:41:42 +0300

    useFetch custom hook

--- src/components/TripList.jsx
+++ src/components/TripList.jsx
@@ -1,30 +1,22 @@
 import { useState, useEffect, useCallback } from 'react'
 import './TripList.css'
+import { useFetch } from '../hooks/useFetch'
 
 export const TripList = () => {
-  const [trips, setTrips] = useState([])
   const [url, setUrl] = useState('http://localhost:3000/trips')
-
-  const fetchTrips = useCallback(async () => {
-    const response = await fetch(url)
-    const json = await response.json()
-    setTrips(json)
-  }, [url])
-
-  useEffect(() => {
-    fetchTrips()
-  }, [url, fetchTrips])
+  const { data } = useFetch(url)
 
   return (
     <div className='trip-list'>
       <h2>Trip list:</h2>
       <ul>
-        {trips.map((trip) => (
-          <li key={trip.id}>
-            <h3>{trip.title}</h3>
-            <p>{trip.price}</p>
-          </li>
-        ))}
+        {data &&
+          data.map((trip) => (
+            <li key={trip.id}>
+              <h3>{trip.title}</h3>
+              <p>{trip.price}</p>
+            </li>
+          ))}
       </ul>
       <div className='filters'>
         <button
