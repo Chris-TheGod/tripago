d2212f6a8eedcdb395b0f1feff181229a30471aa
Kristjan Lääne <xonproject@gmail.com>
2023-09-27 18:25:55 +0300

    abort controller

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -6,11 +6,13 @@ export const useFetch = (url) => {
   const [error, setError] = useState(null)
 
   useEffect(() => {
+    const controller = new AbortController()
+
     const fetchData = async () => {
       setIsPending(true)
 
       try {
-        const res = await fetch(url)
+        const res = await fetch(url, { signal: controller.signal })
         if (!res.ok) {
           throw new Error(res.statusText)
         }
@@ -20,13 +22,21 @@ export const useFetch = (url) => {
         setData(json)
         setError(null)
       } catch (error) {
-        setIsPending(false)
-        setError('Could not fetch the data')
-        console.log(error.message)
+        if (error.name === 'AbortError') {
+          console.log('The fetch was aborted')
+        } else {
+          setIsPending(false)
+          setError('Could not fetch the data')
+          console.log(error.message)
+        }
       }
     }
 
     fetchData()
+
+    return () => {
+      controller.abort()
+    }
   }, [url])
 
   return { data, isPending, error }
