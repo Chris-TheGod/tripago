6044e26c7a1572dcf422cf537a6587b1ec09f43b
Kristjan Lääne <xonproject@gmail.com>
2023-09-27 18:04:25 +0300

    error handling

--- src/components/TripList.jsx
+++ src/components/TripList.jsx
@@ -4,12 +4,13 @@ import { useFetch } from '../hooks/useFetch'
 
 export const TripList = () => {
   const [url, setUrl] = useState('http://localhost:3000/trips')
-  const { data, isPending } = useFetch(url)
+  const { data, isPending, error } = useFetch(url)
 
   return (
     <div className='trip-list'>
       <h2>Trip list:</h2>
       {isPending && <div>Loading trips...</div>}
+      {error && <div>{error}</div>}
       <ul>
         {data &&
           data.map((trip) => (
